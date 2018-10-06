706349c1ecd534318430417c1d59be40341419c4
twairball <twairball@yahoo.com>
2018-10-06 15:18:06 +0800

    fix draw_boxes

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,8 @@ BLUE = (255, 0, 0)
 RED = (0, 0, 255)
 
 def draw_boxes(frame, boxes, color=GREEN):
-    for (x1, y1, x2, y2) in boxes:
-        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
+    for [x1, y1, x2, y2] in boxes:
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
     return frame
 
 def resize_image(image, size_limit=500.0):
